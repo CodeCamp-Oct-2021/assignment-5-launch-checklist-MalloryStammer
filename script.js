a3d6976d2060b7ef5e28cf3e6b2bb797c4302f7a
Mallory Stammer <mallorystammer2@gmail.com>
2021-11-16 16:49:48 -0600

    added notes, started formSubmission function

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  //   let listedPlanets;
+  //   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+  //   let listedPlanetsResponse;
+  //   listedPlanetsResponse
+  //     .then(function (result) {
+  //       listedPlanets = result;
+  //       console.log(listedPlanets);
+  //     })
+  //     .then(function () {
+  //       console.log(listedPlanets);
+  // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+  // });
+  //On Adding Validation
+  //we're already inside window load event listener, so don't make a new one
+  //(you should only ever have one!)
+  //before we worry about validating these inputs, we need to
+  //select the form and listen for the submit event
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    //default behavior is that when we submit HTML forms, the form
+    //sends an HTTP request and refreshes the page
+    //we don't want this to happen, we just want the code in our
+    //form submit event listener to execute
+    event.preventDefault();
+    //prevent default should be the first thing in the form event listener
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    //we now need to validate form inputs using the DOM and some conditionals (formSubmission function)
+    //we are going to use the formSubmission function to validate the inputs
+    //of each of our form input: pilotName, copilotName, fuelLevel, cargoMass
+    //before we can validate the input values we need to select
+    //all of the form values with the DOM  - use crew selector or validatebyid-(look at 25.9.2)
+    //let input1= select an input here
+    
+    
+
+    //we'll want to do this four times, once for each element
+    //now that we have all form input elements selected,
+    //we can pass their VALUES****** into our formSubmission function
+    //very important to understand the difference between an
+    //HTML element and its value!!!  
+    //theres an entire element for pilot name in the HTML called "pilotName"
+    //this is where we will eventually invoke our formSubmission function
+    //alert("Warning: all fields required!")
+   //alert("Please enter text for names and valid integers for fuel and cargo mass!")
+
+  });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -19,8 +19,73 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 function validateInput(testInput) {
    
 }
+// on implementing formSubmission
+//this formSubmission function will be invoked inside the
+//form submit event listener in script.js to validate all inputs
+// has 6 parameters:
+/*  document, which is the document object so you just pass it document as 
+        its first parameter when you invoke it
+    list, which is the faultyItems div
+    pilot, which is the value of our pilotName input
+    copilot, which is the value of our copilotName inpu
+    fuel level, value of fuelLevel input
+    cargoLevel, value of cargoMass input
+
+    pilotName
+    copilotName
+    fuelLevel
+    cargoMass
+*/
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    // given these inputs, what are our conditions?
+    // this should be a sizeable series of if/else blocks
+    /* CONDITION 1: ALL FIELDS REQUIRED!
+        if any value is empty we need to send alert that reads "all fields required!"*/
+    if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+        alert("All fields are required!");
+        } else if (
+        pilotName.value
+        ) 
+
+
+    /* CONDITION 2: use inNaN to make sure data inputted in each field is correct datatype
+        ~ if pilotName or copilotName are numbers, alert "Please enter names, not numbers!
+        ~ if fuel level and/or cargoLevel are not numbers, alert "please enter valid numbers, please!
+        
+        /*refer to the hint on using isNaN function : If you want to check if something is NaN, you 
+        cannot use == or ===. Instead, JavaScript has a built-in method called isNaN(value)
+         that returns true if value is NaN and false if value is not NaN.*/
+   /* based on these conditions, if we have gotten to this point and haven't had to alert the user
+    that they entered bad DataTransfer, we can assume we have validated the input and we are 
+    good to go to start checking our fuel and cargo levels updating our HTML accordingly*/
+    //what if cargoMass <10000 and fuel level is <10000 - fuelLevel is too low, faultyItems list parameter should become visible
+    //what if cargoMass >10000 and fuel level is <10000 - fuelLevel is too low, cargoMass is too high, faultyItems list parameter should become visible
+    //what if cargoMass >10000 and fuel level is >10000 - cargoMass too high, faultyItems list parameter should become visible
+
+
+    // in any of these cases our list parameter (faultyItems) should become visible also the pilotstatus and copilotstatus text should be updated using a template literal that would read something like 'nameOfPilot is ready for launch'
+    //The pilotStatus and copilotStatus text should be updated using a template literal 
+    //that would read something like "nameOfPilot is ready for launch"
+    //How do we update the text/html inside of an HTML element?
+    // Do we already have the pilotStatus and copilotStatus HTML elements selected, or
+    // do we still need to select those to update them?
+
+
+    //if we are ready for launch that means we have enough fuel and our cargo mass is low enough
+    //what if cargoMass <10000 and fuel level is >10000 - perfect!
+
+    //in any other case, we are not ready for launch.
+
+    //we'll need to update the 
+    //launchStatus text and color to reflect whether or not we are ready for launch as well
+    // just like pilotStatus and copilotStatus, do we need to select launchStatus?
+
+    //select all necessary elements BEFORE running the conditionals for organization!
+
+
+
    
 }
 
