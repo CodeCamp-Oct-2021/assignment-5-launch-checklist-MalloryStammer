716f75bad4b19d2cdf11266b458052612308f6fa
Mallory Stammer <mallorystammer2@gmail.com>
2021-11-29 14:50:16 -0600

    almost done, have fetch error

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function () {
-  //   let listedPlanets;
-  //   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-  //   let listedPlanetsResponse;
-  //   listedPlanetsResponse
-  //     .then(function (result) {
-  //       listedPlanets = result;
-  //       console.log(listedPlanets);
-  //     })
-  //     .then(function () {
-  //       console.log(listedPlanets);
-  // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-  // });
-  //On Adding Validation
-  //we're already inside window load event listener, so don't make a new one
-  //(you should only ever have one!)
-  //before we worry about validating these inputs, we need to
-  //select the form and listen for the submit event
-  let form = document.querySelector("form");
-  form.addEventListener("submit", function (event) {
-    //default behavior is that when we submit HTML forms, the form
-    //sends an HTTP request and refreshes the page
-    //we don't want this to happen, we just want the code in our
-    //form submit event listener to execute
-    event.preventDefault();
-    //prevent default should be the first thing in the form event listener
+  let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()** DONE
+  let listedPlanetsResponse = myFetch();
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+      // Why two console logs^^?
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+      let destinationPlanet = pickPlanet(listedPlanets);
 
-    //we now need to validate form inputs using the DOM and some conditionals (formSubmission function)
-    //we are going to use the formSubmission function to validate the inputs
-    //of each of our form input: pilotName, copilotName, fuelLevel, cargoMass
-    //before we can validate the input values we need to select
-    //all of the form values with the DOM  - use crew selector or validatebyid-(look at 25.9.2)
-    //let input1= select an input here
-    
-    
+      missionTarget.style.visibility = "visible";
 
-    //we'll want to do this four times, once for each element
-    //now that we have all form input elements selected,
-    //we can pass their VALUES****** into our formSubmission function
-    //very important to understand the difference between an
-    //HTML element and its value!!!  
-    //theres an entire element for pilot name in the HTML called "pilotName"
-    //this is where we will eventually invoke our formSubmission function
-    //alert("Warning: all fields required!")
-   //alert("Please enter text for names and valid integers for fuel and cargo mass!")
+      missionTarget.innerHTML = `<h2>Mission Destination</h2>
+        <ol>
+            <li>Name: ${destinationPlanet.name}</li>
+            <li>Diameter: ${destinationPlanet.diameter}</li>
+            <li>Star: ${destinationPlanet.star}</li>
+            <li>Distance from Earth: ${destinationPlanet.distance}</li>
+            <li>Number of Moons: ${destinationPlanet.moons}</li>
+        </ol>
+        <img src="${destinationPlanet.image}">`;
+    });
+});
 
-  });
+//On Adding Validation
+//we're already inside window load event listener, so don't make a new one
+//(you should only ever have one!)
+//before we worry about validating these inputs, we need to
+//select the form and listen for the submit event
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //prevent default should be the first thing in the form event listener
+  let pilotNameVar = document.getElementById("pilotName").value;
+  let copilotNameVar = document.querySelector("input[name= copilotName]").value;
+  let fuelLevelVar = document.querySelector("input[name= fuelLevel]").value;
+  let cargoMassVar = document.querySelector("input[name= cargoMass]").value;
+  let faultyItemsVar = document.getElementById("faultyItems");
+  formSubmission(
+    document,
+    faultyItemsVar,
+    pilotNameVar,
+    copilotNameVar,
+    fuelLevelVar,
+    cargoMassVar
+  );
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,108 +1,92 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+require("isomorphic-fetch");
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
-
-function validateInput(testInput) {
-   
-}
-// on implementing formSubmission
-//this formSubmission function will be invoked inside the
-//form submit event listener in script.js to validate all inputs
-// has 6 parameters:
-/*  document, which is the document object so you just pass it document as 
-        its first parameter when you invoke it
-    list, which is the faultyItems div
-    pilot, which is the value of our pilotName input
-    copilot, which is the value of our copilotName inpu
-    fuel level, value of fuelLevel input
-    cargoLevel, value of cargoMass input
-
-    pilotName
-    copilotName
-    fuelLevel
-    cargoMass
-*/
-
+function addDestinationInfo(
+  document,
+  name,
+  diameter,
+  star,
+  distance,
+  moons,
+  imageUrl
+) {}
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    // given these inputs, what are our conditions?
-    // this should be a sizeable series of if/else blocks
-    /* CONDITION 1: ALL FIELDS REQUIRED!
-        if any value is empty we need to send alert that reads "all fields required!"*/
-    if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-        alert("All fields are required!");
-        } else if (
-        pilotName.value
-        ) 
-
-
-    /* CONDITION 2: use inNaN to make sure data inputted in each field is correct datatype
-        ~ if pilotName or copilotName are numbers, alert "Please enter names, not numbers!
-        ~ if fuel level and/or cargoLevel are not numbers, alert "please enter valid numbers, please!
-        
-        /*refer to the hint on using isNaN function : If you want to check if something is NaN, you 
-        cannot use == or ===. Instead, JavaScript has a built-in method called isNaN(value)
-         that returns true if value is NaN and false if value is not NaN.*/
-   /* based on these conditions, if we have gotten to this point and haven't had to alert the user
-    that they entered bad DataTransfer, we can assume we have validated the input and we are 
-    good to go to start checking our fuel and cargo levels updating our HTML accordingly*/
-    //what if cargoMass <10000 and fuel level is <10000 - fuelLevel is too low, faultyItems list parameter should become visible
-    //what if cargoMass >10000 and fuel level is <10000 - fuelLevel is too low, cargoMass is too high, faultyItems list parameter should become visible
-    //what if cargoMass >10000 and fuel level is >10000 - cargoMass too high, faultyItems list parameter should become visible
-
-
-    // in any of these cases our list parameter (faultyItems) should become visible also the pilotstatus and copilotstatus text should be updated using a template literal that would read something like 'nameOfPilot is ready for launch'
-    //The pilotStatus and copilotStatus text should be updated using a template literal 
-    //that would read something like "nameOfPilot is ready for launch"
-    //How do we update the text/html inside of an HTML element?
-    // Do we already have the pilotStatus and copilotStatus HTML elements selected, or
-    // do we still need to select those to update them?
-
-
-    //if we are ready for launch that means we have enough fuel and our cargo mass is low enough
-    //what if cargoMass <10000 and fuel level is >10000 - perfect!
-
-    //in any other case, we are not ready for launch.
-
-    //we'll need to update the 
-    //launchStatus text and color to reflect whether or not we are ready for launch as well
-    // just like pilotStatus and copilotStatus, do we need to select launchStatus?
-
-    //select all necessary elements BEFORE running the conditionals for organization!
-
-
-
-   
+  let alertRequired = "All fields are required!";
+  let alertValid =
+    "Please enter valid text for names and integers for fuel and cargo mass!";
+  let faultyItemsVar = document.getElementById("faultyItems");
+  let launchStatusVar = document.getElementById("launchStatus");
+  let pilotStatusVar = document.getElementById("pilotStatus");
+  let copilotStatusVar = document.getElementById("copilotStatus");
+  let fuelStatusVar = document.getElementById("fuelStatus");
+  let cargoStatusVar = document.getElementById("cargoStatus");
+  if (pilot === "" || copilot === "" || fuelLevel === "" || cargoLevel === "") {
+    alert(alertRequired);
+  } else if (isNaN(fuelLevel) || isNaN(cargoLevel)) {
+    alert(alertValid);
+  } else if (!isNaN(pilot) || !isNaN(copilot)) {
+    alert(alertValid);
+  } else if (cargoLevel < 10000 && fuelLevel < 10000) {
+    list.style.visibility = "visible";
+    launchStatusVar.innerHTML = "Shuttle not ready for launch";
+    launchStatusVar.style.color = "red";
+    pilotStatusVar.innerHTML = `${pilot} is ready for launch`;
+    copilotStatusVar.innerHTML = `${copilot} is ready for launch`;
+    fuelStatusVar.innerHTML = `${fuelLevel} is too low to launch`;
+    cargoStatusVar.innerHTML = `${cargoLevel} is within range and ready to launch`;
+  } else if (cargoLevel > 10000 && fuelLevel < 10000) {
+    list.style.visibility = "visible";
+    launchStatusVar.innerHTML = "Shuttle not ready for launch";
+    launchStatusVar.style.color = "red";
+    pilotStatusVar.innerHTML = `${pilot} is ready for launch`;
+    copilotStatusVar.innerHTML = `${copilot} is ready for launch`;
+    fuelStatusVar.innerHTML = `${fuelLevel} is too low to launch`;
+    cargoStatusVar.innerHTML = `${cargoLevel} is too high to launch`;
+  } else if (cargoLevel > 10000 && fuelLevel > 10000) {
+    list.style.visibility = "visible";
+    launchStatusVar.innerHTML = "Shuttle not ready for launch";
+    launchStatusVar.style.color = "red";
+    pilotStatusVar.innerHTML = `${pilot} is ready for launch`;
+    copilotStatusVar.innerHTML = `${copilot} is ready for launch`;
+    fuelStatusVar.innerHTML = `${fuelLevel} is within range and ready to launch`;
+    cargoStatusVar.innerHTML = `${cargoLevel} is too high to launch`;
+  } else if (cargoLevel < 10000 && fuelLevel > 10000) {
+    /*everything ready and good*/
+    list.style.visibility = "visible";
+    launchStatusVar.innerHTML = "Shuttle is ready for launch";
+    launchStatusVar.style.color = "green";
+    //could i just make these elements below visible? This text is in HTML
+    pilotStatusVar.innerHTML = `${pilot} is ready for launch`;
+    copilotStatusVar.innerHTML = `${copilot} is ready for launch`;
+    fuelStatusVar.innerHTML = `${fuelLevel} is within range and ready to launch`;
+    cargoStatusVar.innerHTML = `${cargoLevel} is within range and ready to launch`;
+  }
 }
 
 async function myFetch() {
-    let planetsReturned;
+  let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+  planetsReturned = await fetch(
+    "https://handlers.education.launchcode.org/static/planets.json"
+  ).then(function (response) {
+    return response.json();
+  });
 
-    return planetsReturned;
+  return planetsReturned;
 }
 
-function pickPlanet(planets) {
+function pickPlanet(listedPlanets) {
+  let randomIndex = Math.floor(
+    Math.random(listedPlanets) * listedPlanets.length
+  );
+  return listedPlanets[randomIndex];
 }
+
+//use innerHTML
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
